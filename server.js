5442e4ce310b0e3887da2108b46a28e1b153ba63
OvernightSolo <sterlingtx75@gmail.com>
2022-10-30 00:46:16 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(require("./routes"));
+
+// Connect mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb.//localhost/social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Log mongo queries that are executed
+mongoose.set("debug", true);
+
+app.listen(PORT, () =>
+  console.log(` ************** Connected on localhost:${PORT} ************* `)
+);
